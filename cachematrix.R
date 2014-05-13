2a776604b5da07b64b4565cd5c06b77761a94ab8
mnicola76 <mnicola76@gmail.com>
2014-05-13 12:05:10 +1000

    Initial Commit containing majority of changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+## This function implements a list with 4 sub functions to...
+## 1. set the pre-calculated matrix
+## 2. get the pre-calculated matrix
+## 3. set the inverse (via solve function) of the matrix
+## 4. get the inverse of the matrix
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Reset free variable m
+    ## m will contain the inversed matrix
+    m <- NULL
+    
+    ## Declare subfunction set
+    ## - 
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    ## Declare subfunction get
+    ## - return pre-calculated matrix
+    get <- function() x
+    
+    ## Declare subfunction setsolve
+    ## - assign/set inverse matrix to m
+    setsolve <- function(solve) m <<- solve
+    
+    ## Declare subfunction getsolve
+    ## - return m (inverse matrix)
+    getsolve <- function() m
+    
+    ## Finally, create list with 
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  
 }
 
-
-## Write a short comment describing this function
-
+## This function accepts a list and returns  
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+
+    ## Firstly, attempt to retrieve from "cache" using getsolve function
+    m <- x$getsolve()
+    ## If we've found something (i.e. not null), then...
+    ## ... return matrix with msg that cache lookup has been successful
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    ## Otherwise, obtain the matrix from the get function...
+    data <- x$get()
+    ## and pass it to solve function to calculate inverse
+    m <- solve(data, ...)
+    ## finally, save to "cache" using setsolve function... 
+    x$setsolve(m)
+    ## ..and return inversed matrix
+    m    
 }
